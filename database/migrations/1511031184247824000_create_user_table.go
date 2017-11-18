fcc66ff8005d6455cbe98aab62572783f4be655a
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2017-11-18 22:41:32 +0330

    Create user migration

---
+++ database/migrations/1511031184247824000_create_user_table.go
@@ -0,0 +1,42 @@
+package migrations
+
+import "log"
+import "gogit/database"
+import "gogit/models"
+
+// CreateUserTable Migration Struct
+type CreateUserTable struct{}
+
+// Migrate the database to a new version
+func (CreateUserTable) Migrate() {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e := db.Model(&models.User{}).Exec(`
+    CREATE TABLE IF NOT EXISTS ?TableName (
+    id bigserial NOT NULL,
+    username varchar(120) NOT NULL,
+    email varchar(255) NOT NULL,
+    fname varchar(255),
+    lname varchar(255),
+    CONSTRAINT public_users_pkey PRIMARY KEY (id),
+    CONSTRAINT public_users_username_key UNIQUE (username),
+    CONSTRAINT public_users_email_key UNIQUE (email)
+    )
+    WITH (OIDS = FALSE)
+    TABLESPACE pg_default;`)
+	if e != nil {
+		log.Println(e)
+	}
+
+}
+
+// Rollback the database to previous version
+func (CreateUserTable) Rollback() {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e := db.Model(&models.User{}).Exec("DROP TABLE ?TableName CASCADE;")
+	if e != nil {
+		log.Println(e)
+	}
+
+}

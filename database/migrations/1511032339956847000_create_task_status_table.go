dc1d7757ccde1ddc8d0774800b18a45b53a2c593
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2017-11-18 22:57:39 +0330

    Create task status migration

---
+++ database/migrations/1511032339956847000_create_task_status_table.go
@@ -0,0 +1,37 @@
+package migrations
+
+import "log"
+import "gogit/database"
+import "gogit/models"
+
+// CreateTaskStatusTable Migration Struct
+type CreateTaskStatusTable struct{}
+
+// Migrate the database to a new version
+func (CreateTaskStatusTable) Migrate() {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e := db.Model(&models.TaskStatus{}).Exec(`CREATE TABLE IF NOT EXISTS ?TableName(
+    id serial NOT NULL,
+    title varchar(255),
+    created_at timestamp with time zone DEFAULT CURRECT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRECT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+    CONSTRAINT task_statuses_pkey PRIMARY KEY (id)
+    )`)
+	if e != nil {
+		log.Println(e)
+	}
+
+}
+
+// Rollback the database to previous version
+func (CreateTaskStatusTable) Rollback() {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e := db.Model(&models.TaskStatus{}).Exec("DROP TABLE IF EXISTS ?TableName CASCADE;")
+	if e != nil {
+		log.Println(e)
+	}
+
+}

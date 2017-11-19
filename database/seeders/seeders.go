88f793dc1750fd880868d6d68889c22e63f1399a
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2017-11-19 07:12:24 +0330

    Set nexval of auto increments

--- database/seeders/seeders.go
+++ database/seeders/seeders.go
@@ -21,20 +21,31 @@ func Seed() {
 	taskStatusSeeder(db)
 }
 
+func setSeqID(model interface{}) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	db.OnQueryProcessed(func(ev *pg.QueryProcessedEvent) {
+		// log.Println(ev.FormattedQuery())
+	})
+	db.Model(model).Exec("SELECT setval('?TableName' || '_id_seq', max(id)) from ?TableName;")
+}
+
 func taskStatusSeeder(db *pg.DB) {
 	_, e = db.Model(&models.TaskStatus{}).Exec(`INSERT INTO ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (1, 'Not started', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
 	if e != nil {
 		log.Println(e)
 	}
-	_, e = db.Model(&models.TaskStatus{}).Exec(`INSERT INTO ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (1, 'In Progress', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
+	_, e = db.Model(&models.TaskStatus{}).Exec(`INSERT INTO ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (2, 'In Progress', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
 	if e != nil {
 		log.Println(e)
 	}
-	_, e = db.Model(&models.TaskStatus{}).Exec(`INSERT INTO ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (1, 'Completed', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
+	_, e = db.Model(&models.TaskStatus{}).Exec(`INSERT INTO ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (3, 'Completed', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
 	if e != nil {
 		log.Println(e)
 	}
-	// db.Model(&models.TaskStatus{}).Exec(`INSERT INTO ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (1, 'Completed', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
+	db.Model(&models.TaskStatus{}).Exec(`SELECT set_val ?TableName (id, title, created_at, updated_at, deleted_at) VALUES (1, 'Completed', now() , now() , null) ON CONFLICT (id) DO NOTHING`)
+
+	setSeqID(&models.TaskStatus{})
 
 }
 
@@ -59,4 +70,7 @@ func taskTypeSeeder(db *pg.DB) {
 	if e != nil {
 		log.Println(e)
 	}
+
+	setSeqID(&models.TaskType{})
+
 }

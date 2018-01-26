daa52ed70a535b155cb7b88f32bbf3e92bc81c51
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-01-26 17:22:39 +0330

    Add calendar system model

---
+++ models/calendar_system.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+)
+
+type CalendarSystem struct {
+	tableName     struct{} `sql:"public.calendar_systems"`
+	Name          string
+	ComponentName string `doc:"UI component name to use for rendering UI date picker. e.g jalali-datepicker"`
+	FilterName    string `doc:"UI filter name to use in template rendering. e.g jdate"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     *time.Time
+}

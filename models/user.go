2ef6d140509f6e5726ac249e4eb6d902413374e1
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-01-26 17:23:14 +0330

    Improve user model

--- models/user.go
+++ models/user.go
@@ -8,8 +8,13 @@ import (
 type User struct {
 	tableName struct{} `sql:"public.users"`
 	ID        uint64
-	FName     string
-	LName     string
+	Username  string ``
+	Email     string ``
+	UserType  uint   `doc:"1: authenticatable user, 2: company"`
+	Firstname string ``
+	Lastname  string ``
+	Companies []User ``
+	Avatar    string ``
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time

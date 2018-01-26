1c0f4dba725de5ea108957e0f0a7d75798bc94db
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-01-26 17:23:04 +0330

    Add local column

--- models/address_country.go
+++ models/address_country.go
@@ -12,6 +12,7 @@ type Country struct {
 	Alpha2Code  string `doc:"ISO Alpha2 Code standard. e.g IR for IRAN"`
 	Alpha3Code  string `doc:"ISO Alpha3 Code standard. e.g IRN for IRAN"`
 	Flag        string `doc:"Base64 string of country flag"`
+	LocaleCode  string `doc:"Localization (i18n) code e.g fa_IR for irannian persian language"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	DeletedAt   *time.Time

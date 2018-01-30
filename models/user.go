6d5781ee42347177e0f2e650900bed31e2590502
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-01-30 06:03:48 +0330

    Improve User mode

--- models/user.go
+++ models/user.go
@@ -6,16 +6,18 @@ import (
 
 // User : model of all system users
 type User struct {
-	tableName struct{} `sql:"public.users"`
-	ID        uint64
-	Username  string ``
-	Email     string ``
-	UserType  uint   `doc:"1: authenticatable user, 2: company"`
-	Firstname string ``
-	Lastname  string ``
-	Companies []User ``
-	Avatar    string ``
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time
+	tableName          struct{} `sql:"public.users"`
+	ID                 uint64
+	Username           string                ``
+	Email              string                ``
+	UserType           uint                  `doc:"1: authenticatable user, 2: company"`
+	Firstname          string                ``
+	Lastname           string                ``
+	UserCompanyMapping []*UserCompanyMapping `doc:"نگاشت کاربران عضو در هر کمپانی"`
+	Avatar             string                ``
+	OwnerID            uint64                `doc:"کد یکتای مالک و سازنده ی یک کمپانی. این فیلد برای حساب کاربری افراد میتواند خالی باشد."`
+	Owner              *User
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+	DeletedAt          *time.Time
 }

7ff5a5cc2bdb01847d2101d0761a9e181e6b3faa
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-01-31 06:45:35 +0330

    Add milestone models

---
+++ models/milestone.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+)
+
+type Milestone struct {
+	ID               uint64
+	Name             string
+	DueDate          time.Time           `doc:"تاریخ دستیابی به هدف"`
+	Description      string              `doc:"Full description about the milestone"`
+	ResponsibleUsers []*User             `pg:"many2many:milestone_responsibale_users"`
+	Followers        []*User             `pg:"many2many:milestone_followers"`
+	Tags             []*MilestoneTag     `doc:"hasMany"`
+	Comments         []*MilestoneComment `doc:"hasMany"`
+	TaskLists        []*TaskList         `pg:"many2many:milestone_tasklists"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	DeletedAt        *time.Time
+}

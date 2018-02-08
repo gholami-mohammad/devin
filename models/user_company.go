7cd5096a8736c9334bb4f512c9ea8d608ed03079
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-08 20:00:39 +0330

    Add user-company permissions

--- models/user_company.go
+++ models/user_company.go
@@ -5,15 +5,18 @@ import (
 )
 
 type UserCompany struct {
-	tableName   struct{} `sql:"user_company_mappings"`
-	ID          uint64
-	UserID      uint64 `doc:"ID of users record with type=1"`
-	User        *User
-	CompanyID   uint64 `doc:"ID of users record with type=2"`
-	Company     *User
-	CreatedByID uint64 `doc:"Who add this user to this company? OR How create this company for this user?"`
-	CreatedBy   *User
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   *time.Time
+	tableName           struct{} `sql:"user_company_mappings"`
+	ID                  uint64
+	UserID              uint64 `doc:"ID of users record with type=1"`
+	User                *User
+	CompanyID           uint64 `doc:"ID of users record with type=2"`
+	Company             *User
+	CreatedByID         uint64 `doc:"Who add this user to this company? OR How create this company for this user?"`
+	CreatedBy           *User
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	DeletedAt           *time.Time
+	IsAdmin             bool `doc:"If is_admin=true => user will has full access"`
+	CanCreateProject    bool
+	CanAddUserToCompany bool
 }

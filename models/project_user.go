63b4aa2289b9c4117b6bd64d65463c9d09bcdcbb
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-08 20:00:25 +0330

    Add project-user permissions

--- models/project_user.go
+++ models/project_user.go
@@ -5,15 +5,29 @@ import (
 )
 
 type ProjectUser struct {
-	tableName   struct{} `sql:"peoject_users"`
-	ID          uint64
-	UserID      uint64 `doc:"ID of users record with type=1"`
-	User        *User
-	ProjectID   uint64 `doc:"ID of pm.peojects record"`
-	Project     *Project
-	CreatedByID uint64 `doc:"Who add this user to this project?"`
-	CreatedBy   *User
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   *time.Time
+	tableName               struct{} `sql:"peoject_users"`
+	ID                      uint64
+	UserID                  uint64 `doc:"ID of users record with type=1"`
+	User                    *User
+	ProjectID               uint64 `doc:"ID of pm.peojects record"`
+	Project                 *Project
+	CreatedByID             uint64 `doc:"Who add this user to this project?"`
+	CreatedBy               *User
+	CreatedAt               time.Time
+	UpdatedAt               time.Time
+	DeletedAt               *time.Time
+	IsAdmin                 bool `doc:"Full access to all project features"`
+	CanUpdateProjectProfile bool `doc:"امکان ویرایش اطلاعات پروژه"`
+	CanAddUserToProject     bool
+	CanCreateMilestone      bool
+	CanCreateTaskList       bool
+	CanCreateTask           bool
+	CanCreateIssue          bool
+	CanCreateRepository     bool
+	CanCreateTag            bool
+	CanCreateBoard          bool
+	CanCreateReminder       bool
+	CanCreateTimeLog        bool `doc:"Log times spend on task o project"`
+	CanListAllTimeLogs      bool
+	CanCreateWiki           bool
 }

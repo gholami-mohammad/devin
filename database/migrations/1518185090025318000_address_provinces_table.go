f2231ab8345e56e2b3a5b071153b1e26cd6a28bf
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-09 17:48:28 +0330

    Add address provinces migration

---
+++ database/migrations/1518185090025318000_address_provinces_table.go
@@ -0,0 +1,35 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateAddressProvincesTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.provinces (
+    id serial NOT NULL,
+    name varchar(255),
+    country_id integer NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+
+    CONSTRAINT address_provinces_pkey PRIMARY KEY (id),
+    CONSTRAINT address_provinces_id_address_country_id FOREIGN KEY (country_id)
+        REFERENCES public.address_countries (id) MATCH SIMPLE
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+
+    );`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackAddressProvincesTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.provinces;")
+
+	return
+}

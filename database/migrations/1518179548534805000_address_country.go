dd431f020aa7780d47010cbde105fa3e2827f74c
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-09 17:28:07 +0330

    Create country migration

---
+++ database/migrations/1518179548534805000_address_country.go
@@ -0,0 +1,32 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateAddressCountry() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS address_countries (
+    id serial NOT NULL,
+    name varchar(100) NOT NULL,
+    phone_Prefix varchar(3),
+    alpha2_code varchar(2),
+    alpha3_code varchar(3),
+    flag text,
+    locale_code varchar(6),
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+    CONSTRAINT address_countries_pkey PRIMARY KEY (id)
+    );`)
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackAddressCountry() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("")
+
+	return
+}

cb3b515277dad94441c36a6730ac6a34ff205e60
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-13 06:46:28 +0330

    Create project statuses table migration

---
+++ database/migrations/1518491451187636000_project_status_table.go
@@ -0,0 +1,29 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateProjectStatusTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.project_statuses(
+    id serial NOT NULL,
+    status varchar(50) NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+
+    CONSTRAINT project_statuses_pkey PRIMARY KEY(id)
+    )`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackProjectStatusTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.project_statuses;")
+
+	return
+}
--- models/project_status.go
+++ models/project_status.go
@@ -4,6 +4,7 @@ import "time"
 
 //ProjectStatus : active, archived, pending, etc
 type ProjectStatus struct {
+	tableName struct{} `sql:"project_statuses"`
 	ID        uint
 	Status    string
 	CreatedAt time.Time

4ccb10d1bb2fdb4b78904d3a4affc757bd90650b
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-24 20:41:49 +0330

    Add milestone fk and pk

--- database/migrations/1519317805098759000_milestones_table.go
+++ database/migrations/1519317805098759000_milestones_table.go
@@ -8,14 +8,20 @@ func (Migration) MigrateMilestonesTable() (e error) {
 	defer db.Close()
 	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.milestones (
     id bigserial NOT NULL,
-    name string NOT NULL,
+    name varchar(255) NOT NULL,
     due_date timestamp with time zone,
     description text,
     created_by_id bigint,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     deleted_at timestamp with time zone,
-    )`)
+
+    CONSTRAINT milestones_pkey PRIMARY KEY (id),
+    CONSTRAINT milestones_created_by_id_users_id FOREIGN KEY (created_by_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    );`)
 
 	return
 }

7facf2c219fa5389ee8ac3922707a0df0aaa05e3
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-02-27 06:53:52 +0330

    create task_priopities migration

---
+++ database/migrations/1519701550543415000_task_priorities_table.go
@@ -0,0 +1,30 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateTaskPrioritiesTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.task_priopities(
+    id serial NOT NULL,
+    title varchar(255) NOT NULL,
+    color varchar(7),
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+
+    CONSTRAINT task_priorities_pkey PRIMARY KEY (id)
+    );`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackTaskPrioritiesTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.task_priorities CASCADE;")
+
+	return
+}
--- models/task_priority.go
+++ models/task_priority.go
@@ -4,8 +4,10 @@ import "time"
 
 // TaskPriority : None , Low , Medium , High
 type TaskPriority struct {
+	tableName struct{} `sql:"public.task_priopities"`
 	ID        uint
 	Title     string
+	Color     string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time

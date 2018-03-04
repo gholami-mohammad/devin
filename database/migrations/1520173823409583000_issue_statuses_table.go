93f7ce7248de372f4d8002ae045703b33e419860
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-04 18:17:33 +0330

    Create issue_statues migration

---
+++ database/migrations/1520173823409583000_issue_statuses_table.go
@@ -0,0 +1,27 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateIssueStatusesTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.issue_statuses(
+    id serial NOT NULL,
+    title varchar(120) NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone
+    )`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackIssueStatusesTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.issue_statuses;")
+
+	return
+}
--- models/issue_status.go
+++ models/issue_status.go
@@ -10,11 +10,9 @@ import "time"
 //
 // closed : مساله بررسی و به نتیجه رسید که تنیجه نهایی آن در صورتیکه مساله غیر گیت باشد باید مشروح نوشته شود و سپس بسته شود. در مسايل گیت غالبا یا با آپدیت کد مشکل برطرف میشود که در این حالت مسئله به لیست باگ ها منتقل میشود و از کانالی دیگر پیگیری میشود و یا اینکه نیاز به راهنمایی جهت حل مشکل در بخشی است که در کامنت های افراد توضیحات یافت میشود.
 type IssueStatus struct {
-	ID                      uint
-	Title                   string
-	SetAsInProgressDateTime *time.Time
-	SetAsResolvedDateTime   *time.Time
-	CreatedAt               time.Time
-	UpdatedAt               time.Time
-	DeletedAt               *time.Time
+	ID        uint
+	Title     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
 }

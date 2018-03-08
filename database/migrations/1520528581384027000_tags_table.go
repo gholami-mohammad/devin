ff5432475825c8d74303f7bf01846591dd7b88ca
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-08 20:39:39 +0330

    Create tags migration

---
+++ database/migrations/1520528581384027000_tags_table.go
@@ -0,0 +1,39 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateTagsTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.tags (
+    id bigserial NOT NULL,
+    company_id bigint NOT NULL,
+    title varchar(255),
+    created_by_id bigint NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+
+    CONSTRAINT tags_pkey PRIMARY KEY (id),
+    CONSTRAINT tags_company_id_users_id FOREIGN KEY (company_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT tags_created_by_id_users_id FOREIGN KEY (created_by_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackTagsTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.tags CASCADE;")
+
+	return
+}
--- models/tag.go
+++ models/tag.go
@@ -5,10 +5,11 @@ import (
 )
 
 type Tag struct {
+	tableName   struct{} `sql:"public.tags"`
 	ID          uint64
-	Title       string
-	CompanyID   uint64 `doc:"This tag created on this company. All tags of a company will be shared in all of its projects and other modules"`
+	CompanyID   uint64 `doc:"This tag created on this company. All tags of a company will be shared in all of its projects and modules"`
 	Company     *User
+	Title       string
 	CreatedByID uint64 `doc:"Who creates this tag?"`
 	CreatedBy   *User
 	CreatedAt   time.Time

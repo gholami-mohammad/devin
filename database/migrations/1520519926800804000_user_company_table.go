1d99e0d913281798a8d788d0ea752d2776beea7e
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-08 18:18:01 +0330

    Create user_company migration

---
+++ database/migrations/1520519926800804000_user_company_table.go
@@ -0,0 +1,46 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateUserCompanyTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.user_company (
+    id bigserial NOT NULL,
+    user_id bigint NOT NULL,
+    company_id bigint NOT NULL,
+    is_admin bool DEFAULT false,
+    can_create_project bool DEFAULT false,
+    can_add_user_to_company bool DEFAULT false,
+    created_by_id bigint NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp with time zone,
+
+    CONSTRAINT user_company_pkey PRIMARY KEY (id),
+    CONSTRAINT user_company_user_id_users_id FOREIGN KEY (user_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT user_company_company_id_users_id FOREIGN KEY (company_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT user_company_created_by_id_users_id FOREIGN KEY (created_by_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackUserCompanyTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.user_company;")
+
+	return
+}
--- models/user_company.go
+++ models/user_company.go
@@ -5,18 +5,18 @@ import (
 )
 
 type UserCompany struct {
-	tableName           struct{} `sql:"user_company_mappings"`
+	tableName           struct{} `sql:"public.user_company"`
 	ID                  uint64
 	UserID              uint64 `doc:"ID of users record with type=1"`
 	User                *User
 	CompanyID           uint64 `doc:"ID of users record with type=2"`
 	Company             *User
+	IsAdmin             bool `doc:"If is_admin=true => user will has full access"`
+	CanCreateProject    bool
+	CanAddUserToCompany bool
 	CreatedByID         uint64 `doc:"Who add this user to this company? OR How create this company for this user?"`
 	CreatedBy           *User
 	CreatedAt           time.Time
 	UpdatedAt           time.Time
 	DeletedAt           *time.Time
-	IsAdmin             bool `doc:"If is_admin=true => user will has full access"`
-	CanCreateProject    bool
-	CanAddUserToCompany bool
 }

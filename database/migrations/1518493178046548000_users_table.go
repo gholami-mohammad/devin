2c830dd0be4cda9a6da6c9a2ff6a152ae5e4fa13
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-08 19:10:07 +0330

    Add user profile section to user tabel

--- database/migrations/1518493178046548000_users_table.go
+++ database/migrations/1518493178046548000_users_table.go
@@ -15,6 +15,25 @@ func (Migration) MigrateUsersTable() (e error) {
     user_type integer,
     avatar varchar(200),
     owner_id bigint,
+
+    job_title varchar(150),
+    localization_language_id integer,
+    date_format varchar(100),
+    time_format varchar(100),
+    calendar_system_id integer,
+    office_phone_country_code_id integer,
+    home_phone_country_code_id integer,
+    cell_phone_country_code_id integer,
+    country_id integer,
+    province_id integer,
+    city_id integer,
+    twitter varchar(300),
+    linkedin varchar(300),
+    google_plus varchar(300),
+    facebook varchar(300),
+    telegram varchar(300),
+    website varchar(300),
+
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     deleted_at timestamp with time zone,
@@ -25,7 +44,39 @@ func (Migration) MigrateUsersTable() (e error) {
     CONSTRAINT users_owner_id_users_id FOREIGN KEY (owner_id)
         REFERENCES public.users (id)
         ON DELETE CASCADE
-        ON UPDATE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_localization_language_id_address_countries_id FOREIGN KEY (localization_language_id)
+        REFERENCES public.address_countries (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_calendar_system_id_calendar_systems_id FOREIGN KEY (calendar_system_id)
+        REFERENCES public.calendar_systems (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_office_phone_country_code_id_address_countries_id FOREIGN KEY (office_phone_country_code_id)
+        REFERENCES public.address_countries (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_home_phone_country_code_id_address_countries_id FOREIGN KEY (home_phone_country_code_id)
+        REFERENCES public.address_countries (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_cell_phone_country_code_id_address_countries_id FOREIGN KEY (cell_phone_country_code_id)
+        REFERENCES public.address_countries (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_country_id_address_countries_id FOREIGN KEY (country_id)
+        REFERENCES public.address_countries (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_province_id_address_provinces_id FOREIGN KEY (province_id)
+        REFERENCES public.address_provinces (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT users_city_id_address_cities_id FOREIGN KEY (city_id)
+        REFERENCES public.address_cities (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     );`)
 
 	return
--- models/user.go
+++ models/user.go
@@ -17,7 +17,39 @@ type User struct {
 	Avatar             string
 	OwnerID            uint64 `doc:"کد یکتای مالک و سازنده ی یک کمپانی. این فیلد برای حساب کاربری افراد میتواند خالی باشد."`
 	Owner              *User
-	CreatedAt          time.Time
-	UpdatedAt          time.Time
-	DeletedAt          *time.Time
+
+	/**
+	 * Profile properties
+	 */
+	JobTitle                string          `doc:"User's job title in a company"`
+	LocalizationLanguageID  uint            `doc:"FK to countries table to get localization settings"`
+	LocalizationLanguage    *Country        `doc:"Belongs to Country model to load i18n settings"`
+	DateFormat              string          `doc:"Default date formate to show dates in UI. List of date formates stored in 'date_formats' table, but for more DB performance, directly saved here."`
+	TimeFormat              string          `doc:"Default time format to show in UI. Time formats stored in 'time_formats' table, but for more DB performance, directly saved here."`
+	CalendarSystemID        uint            `doc:"FK to calendar_systems"`
+	CalendarSystem          *CalendarSystem `doc:"Which calendar system will used to use in datepicker and showing dates "`
+	OfficePhoneCountyCodeID uint            `doc:"FK to countries table"`
+	OfficePhoneCountryCode  *Country        `doc:"Belogs to Country"`
+	HomePhoneCountyCodeID   uint            `doc:"FK to countries table"`
+	HomePhoneCountryCode    *Country        `doc:"Belogs to Country"`
+	CellPhoneCountyCodeID   uint            `doc:"FK to countries table"`
+	CellPhoneCountryCode    *Country        `doc:"Belogs to Country"`
+	FaxCountyCodeID         uint            `doc:"FK to countries table"`
+	FaxCountryCode          *Country        `doc:"Belogs to Country"`
+	CountryID               uint            `doc:"#Address, FK to countries table. To improve database performance and ignore inner joings on SQL queries to load this data."`
+	Country                 *Country        `doc:"Belogs to Country"`
+	ProvinceID              uint            `doc:"#Address, FK to provinces table. To improve database performance and ignore inner joings on SQL queries to load this data."`
+	Province                *Province       `doc:"Belogs to Province"`
+	CityID                  uint            `doc:"#Address, FK to cities table"`
+	City                    *City           `doc:"Belogs to City"`
+	Twitter                 string          `doc:"Twitter username e.g 'm6devin' or full profile URL like 'https://twitter.com/m6devin'"`
+	Linkedin                string          `doc:"Linkedin full profile URL "`
+	GooglePlus              string          `doc:"Google plus full profile URL"`
+	Facebook                string          `doc:"Facebook username or full profile URL"`
+	Telegram                string          `doc:"Telegram username or full telegram profile URL"`
+	Website                 string          `doc:"Personnal website URL"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
 }

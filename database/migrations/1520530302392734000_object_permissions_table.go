e5a3ad2b6482481eb92f7af694c5b984a44dd5ac
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-08 21:07:22 +0330

    Create object_permissions migration

---
+++ database/migrations/1520530302392734000_object_permissions_table.go
@@ -0,0 +1,42 @@
+package migrations
+
+import "gogit/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateObjectPermissionsTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec(`CREATE TABLE IF NOT EXISTS public.object_permissions (
+    id bigserial NOT NULL ,
+    user_id bigint NOT NULL,
+    object_id bigint NOT NULL,
+    module_id bigint NOT NULL,
+    can_read bool DEFAULT false,
+    can_update bool DEFAULT false,
+    can_delete bool DEFAULT false,
+    created_by_id bigint NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT object_permissions_pkey PRIMARY KEY (id),
+    CONSTRAINT object_permissions_composit_unique UNIQUE (user_id, object_id, module_id),
+    CONSTRAINT object_permissions_user_id_users_id FOREIGN KEY (user_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT object_permissions_created_by_id_users_id FOREIGN KEY (created_by_id)
+        REFERENCES public.users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )`)
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackObjectPermissionsTable() (e error) {
+	db := database.NewPGInstance()
+	defer db.Close()
+	_, e = db.Exec("DROP TABLE IF EXISTS public.object_permissions;")
+
+	return
+}

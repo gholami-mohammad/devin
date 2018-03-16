9e89430da3093157c6e39a2a333c02f37e954158
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-16 21:45:25 +0330

    Load router

--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
-	"gogit/models"
+	user_router "devin/modules/user/routes"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	var a models.City
-	log.Println(a)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hey"))
-	})
-	http.ListenAndServe(":80", nil)
+	r := mux.NewRouter()
+
+	u := r.PathPrefix("/api").Subrouter().StrictSlash(true)
+	user_router.LoadRoutes(u)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":13000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }

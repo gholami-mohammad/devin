cda9c1b54d2ebef35cf81ba3c1a8ce8be71f0f0b
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-16 21:23:25 +0330

    Create function to make errors on response

---
+++ helpers/error_response.go
@@ -0,0 +1,17 @@
+package helpers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	Message   string              `json:"message"`
+	ErrorCode int                 `json:"error_code"`
+	Errors    map[string][]string `json:"errors"`
+}
+
+func NewErrorResponse(w http.ResponseWriter, err *ErrorResponse) error {
+	w.WriteHeader(err.ErrorCode)
+	return json.NewEncoder(w).Encode(&err)
+}

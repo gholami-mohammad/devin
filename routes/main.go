eee4123d763586b8ec10613cede2578968cacaaa
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-03-26 00:14:42 +0430

    Load auth middleware on secure routes Add profile update router

--- routes/main.go
+++ routes/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 
+	"devin/middlewares"
 	user_ctrl "devin/modules/user/controllers"
 )
 
@@ -12,7 +13,9 @@ func LoadRoutes(r *mux.Router) *mux.Router {
 	r.HandleFunc("/signup", user_ctrl.Signup).Methods(http.MethodPost)
 	r.HandleFunc("/signin", user_ctrl.Signin).Methods(http.MethodPost)
 
-	r.HandleFunc("/user/update", user_ctrl.UpdateProfile).Methods(http.MethodPost)
+	secureArea := r.PathPrefix("/").Subrouter().StrictSlash(true)
+	secureArea.Use(middlewares.Authenticate)
+	secureArea.HandleFunc("/user/{id:[0-9]+}/update", user_ctrl.UpdateProfile).Methods(http.MethodPost)
 
 	return r
 }

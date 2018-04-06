1ceffda69bf2a31fcf5ea2a5caf29a28ea2962e6
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-06 11:02:43 +0430

    Fix Typo error

--- database/migrations/1518493178046532000_date_formats_table.go
+++ database/migrations/1518493178046532000_date_formats_table.go
@@ -3,27 +3,27 @@ package migrations
 import "devin/database"
 
 // Migrate the database to a new version
-func (Migration) MigrateTimeFormatesTable() (e error) {
+func (Migration) MigrateDateFormatsTable() (e error) {
 	db := database.NewGORMInstance()
 	defer db.Close()
-	e = db.Exec(`CREATE TABLE IF NOT EXISTS public.time_formates (
+	e = db.Exec(`CREATE TABLE IF NOT EXISTS public.date_formats (
     id serial NOT NULL,
     name varchar (100),
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     deleted_at timestamp with time zone,
 
-    CONSTRAINT time_formates_pkey PRIMARY KEY(id)
+    CONSTRAINT date_formats_pkey PRIMARY KEY(id)
     )`).Error
 
 	return
 }
 
 // Rollback the database to previous version
-func (Migration) RollbackTimeFormatesTable() (e error) {
+func (Migration) RollbackDateFormatsTable() (e error) {
 	db := database.NewGORMInstance()
 	defer db.Close()
-	e = db.Exec("DROP TABLE IF EXISTS public.time_formates;").Error
+	e = db.Exec("DROP TABLE IF EXISTS public.date_formats;").Error
 
 	return
 }
--- database/migrations/1518493178046533000_time_formats_table.go
+++ database/migrations/1518493178046533000_time_formats_table.go
@@ -3,27 +3,27 @@ package migrations
 import "devin/database"
 
 // Migrate the database to a new version
-func (Migration) MigrateDateFormatesTable() (e error) {
+func (Migration) MigrateTimeFormatsTable() (e error) {
 	db := database.NewGORMInstance()
 	defer db.Close()
-	e = db.Exec(`CREATE TABLE IF NOT EXISTS public.date_formats (
+	e = db.Exec(`CREATE TABLE IF NOT EXISTS public.time_formats (
     id serial NOT NULL,
     name varchar (100),
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     deleted_at timestamp with time zone,
 
-    CONSTRAINT date_formats_pkey PRIMARY KEY(id)
+    CONSTRAINT time_formats_pkey PRIMARY KEY(id)
     )`).Error
 
 	return
 }
 
 // Rollback the database to previous version
-func (Migration) RollbackDateFormatesTable() (e error) {
+func (Migration) RollbackTimeFormatsTable() (e error) {
 	db := database.NewGORMInstance()
 	defer db.Close()
-	e = db.Exec("DROP TABLE IF EXISTS public.date_formats;").Error
+	e = db.Exec("DROP TABLE IF EXISTS public.time_formats;").Error
 
 	return
 }

14c02a32abb43d1291f2d797222ad5c410d9a752
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-06 12:17:09 +0430

    Add ID to calendar system model

--- models/calendar_system.go
+++ models/calendar_system.go
@@ -6,6 +6,7 @@ import (
 
 type CalendarSystem struct {
 	tableName     struct{} `sql:"public.calendar_systems"`
+	ID            uint64
 	Name          string
 	ComponentName string `doc:"UI component name to use for rendering UI date picker. e.g jalali-datepicker"`
 	FilterName    string `doc:"UI filter name to use in template rendering. e.g jdate"`

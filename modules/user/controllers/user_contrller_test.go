106f09290939a6abc82deea534c1a8a1d1b62eec
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-13 20:07:39 +0430

    Handle shared errors in usercontroller in single function

--- modules/user/controllers/user_contrller_test.go
+++ modules/user/controllers/user_contrller_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"devin/database"
+	"devin/helpers"
 	"devin/middlewares"
 	"devin/models"
 )
@@ -38,15 +39,24 @@ func deleteTestUser(id int) {
 	db.Exec(`delete from users where id=?;`, id)
 }
 
-func TestUpdateProfile(t *testing.T) {
-	user1, _, tokenString := getValidUser(1, true)
+func TestHandleProfileSharedErrors(t *testing.T) {
+	_, _, tokenString := getValidUser(1, true)
 	_, _, tokenStringStandardUser := getValidUser(2, false)
 	defer deleteTestUser(1)
 	defer deleteTestUser(2)
 
 	route := mux.NewRouter()
 	path := "/user/{id}/update"
-	route.Handle(path, http.HandlerFunc(UpdateProfile))
+	route.Handle(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		db := database.NewGORMInstance()
+		defer db.Close()
+		_, err := handleProfileSharedErrors(r, db)
+		if err == nil {
+			return
+		}
+		helpers.NewErrorResponse(w, err)
+		return
+	}))
 	route.Use(middlewares.Authenticate)
 
 	t.Run("bad content-type", func(t *testing.T) {
@@ -189,6 +199,19 @@ func TestUpdateProfile(t *testing.T) {
 		}
 	})
 
+}
+
+func TestUpdateProfile(t *testing.T) {
+	user1, _, tokenString := getValidUser(1, true)
+	// _, _, tokenStringStandardUser := getValidUser(2, false)
+	defer deleteTestUser(1)
+	// defer deleteTestUser(2)
+
+	route := mux.NewRouter()
+	path := "/user/{id}/update"
+	route.Handle(path, http.HandlerFunc(UpdateProfile))
+	route.Use(middlewares.Authenticate)
+
 	t.Run("Invalid request body", func(t *testing.T) {
 		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "1", 1), strings.NewReader("Bad Request body"))
 
@@ -283,7 +306,7 @@ func TestWhoami(t *testing.T) {
 
 func TestUpdateUsername(t *testing.T) {
 	_, _, tokenString := getValidUser(1, true)
-	_, _, tokenStringStandardUser := getValidUser(2, false)
+	getValidUser(2, false)
 	defer deleteTestUser(1)
 	defer deleteTestUser(2)
 
@@ -291,146 +314,6 @@ func TestUpdateUsername(t *testing.T) {
 	path := "/user/{id}/update_username"
 	route.Handle(path, http.HandlerFunc(UpdateUsername))
 	route.Use(middlewares.Authenticate)
-
-	t.Run("bad content-type", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "1", 1), nil)
-		req.Header.Add("Authorization", tokenString)
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-		if res.StatusCode != http.StatusUnsupportedMediaType {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "Invalid content type") {
-			t.Fatal("Invalid response message")
-		}
-	})
-
-	t.Run("no id variable passed to the mux router", func(t *testing.T) {
-		route := mux.NewRouter()
-		path := "/user/update_username"
-		route.Handle(path, http.HandlerFunc(UpdateUsername))
-		req, _ := http.NewRequest(http.MethodPost, path, nil)
-
-		req.Header.Add("Authorization", tokenString)
-		req.Header.Add("Content-Type", "application/json")
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-		if res.StatusCode != http.StatusUnprocessableEntity {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "Invalid User ID") {
-			t.Fatal("Invalid response message")
-		}
-	})
-
-	t.Run("invalid user_id data type", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "-1", 1), nil)
-
-		req.Header.Add("Authorization", tokenString)
-		req.Header.Add("Content-Type", "application/json")
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-
-		if res.StatusCode != http.StatusUnprocessableEntity {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "Invalid User ID. Just integer values accepted") {
-			t.Fatal("Invalid response message")
-		}
-	})
-
-	t.Run("not exists user in DB", func(t *testing.T) {
-		deleteTestUser(0)
-		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "0", 1), nil)
-
-		req.Header.Add("Authorization", tokenString)
-		req.Header.Add("Content-Type", "application/json")
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-
-		if res.StatusCode != http.StatusInternalServerError {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "Error on loading user data") {
-			t.Fatal("Invalid response message")
-		}
-	})
-
-	t.Run("Authorization token error", func(t *testing.T) {
-		route := mux.NewRouter()
-		route.Handle(path, http.HandlerFunc(UpdateUsername))
-		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "1", 1), nil)
-
-		req.Header.Add("Content-Type", "application/json")
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-
-		if res.StatusCode != http.StatusUnauthorized {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "Auhtentication failed") {
-			t.Fatal("Invalid response message")
-		}
-	})
-
-	t.Run("Access denied", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "1", 1), nil)
-
-		req.Header.Add("Authorization", tokenStringStandardUser)
-		req.Header.Add("Content-Type", "application/json")
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-
-		if res.StatusCode != http.StatusForbidden {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "This action is not allowed for you") {
-			t.Fatal("Invalid response message")
-		}
-	})
-
-	t.Run("Empty Request body", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "1", 1), nil)
-
-		req.Header.Add("Authorization", tokenString)
-		req.Header.Add("Content-Type", "application/json")
-		rr := httptest.NewRecorder()
-		route.ServeHTTP(rr, req)
-		res := rr.Result()
-
-		if res.StatusCode != http.StatusInternalServerError {
-			t.Fatal("Status code not matched. Response is", res.StatusCode)
-		}
-
-		defer res.Body.Close()
-		bts, _ := ioutil.ReadAll(res.Body)
-		if !strings.Contains(string(bts), "Request body cant be empty") {
-			t.Fatal("Invalid response message")
-		}
-	})
 
 	t.Run("Invalid request body", func(t *testing.T) {
 		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "1", 1), strings.NewReader("Bad Request body"))
--- modules/user/controllers/user_controller.go
+++ modules/user/controllers/user_controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 
 	"devin/database"
 	"devin/helpers"
@@ -14,94 +15,94 @@ import (
 	"devin/policies"
 )
 
-// UpdateProfile handle user profile updates.
-// Authorizations handlers loaded in middleware
-// This function get json request of user model and update associated model
-// If requested user_id == logged in user_id => user is trying to update his profile,
-// Else, user is trying to update someone else => need authorization check
-func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+func handleProfileSharedErrors(r *http.Request, db *gorm.DB) (user models.User, err *helpers.ErrorResponse) {
 
 	// Check content type
 	if !helpers.HasJSONRequest(r) {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			Message:   "Invalid content type.",
 			ErrorCode: http.StatusUnsupportedMediaType,
 		}
-		helpers.NewErrorResponse(w, &err)
 		return
 	}
 
-	var user models.User
 	userID, ok := mux.Vars(r)["id"]
 	if !ok {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			Message:   "Invalid User ID.",
 			ErrorCode: http.StatusUnprocessableEntity,
 		}
-		helpers.NewErrorResponse(w, &err)
+
 		return
 	}
 	var e error
 	user.ID, e = strconv.ParseUint(userID, 10, 64)
 	if e != nil {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			Message:   "Invalid User ID. Just integer values accepted",
 			ErrorCode: http.StatusUnprocessableEntity,
 		}
-		helpers.NewErrorResponse(w, &err)
 		return
 	}
 	//Load current user data from DB
-	db := database.NewGORMInstance()
-	defer db.Close()
 	e = db.Where("id=?", user.ID).First(&user).Error
 	if e != nil {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			ErrorCode: http.StatusInternalServerError,
 			Message:   "Error on loading user data.",
 		}
-		log.Println("Error on loading user data,", e)
-		helpers.NewErrorResponse(w, &err)
 
 		return
 	}
 
 	authUser, _, e := user.ExtractUserFromRequestContext(r)
 	if e != nil {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			ErrorCode: http.StatusUnauthorized,
 			Message:   "Auhtentication failed.",
 		}
-		log.Println("Auhtentication failed,", e)
-		helpers.NewErrorResponse(w, &err)
 
 		return
 	}
 
 	if !policies.CanEditUser(authUser, user) {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			ErrorCode: http.StatusForbidden,
 			Message:   "This action is not allowed for you.",
 		}
-		log.Println("This action is not allowed for you.,")
-		helpers.NewErrorResponse(w, &err)
 
 		return
 	}
 
 	if r.Body == nil {
-		err := helpers.ErrorResponse{
+		err = &helpers.ErrorResponse{
 			ErrorCode: http.StatusInternalServerError,
 			Message:   "Request body cant be empty",
 		}
-		log.Println("Request body cant be empty,", e)
-		helpers.NewErrorResponse(w, &err)
 
 		return
 	}
 
+	return
+
+}
+
+// UpdateProfile handle user profile updates.
+// Authorizations handlers loaded in middleware
+// This function get json request of user model and update associated model
+// If requested user_id == logged in user_id => user is trying to update his profile,
+// Else, user is trying to update someone else => need authorization check
+func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+	db := database.NewGORMInstance()
+	defer db.Close()
+	user, err := handleProfileSharedErrors(r, db)
+	if err != nil {
+		helpers.NewErrorResponse(w, err)
+		return
+	}
+
 	var profile models.PublicProfile
-	e = json.NewDecoder(r.Body).Decode(&profile)
+	e := json.NewDecoder(r.Body).Decode(&profile)
 	if e != nil {
 		err := helpers.ErrorResponse{
 			ErrorCode: http.StatusInternalServerError,
@@ -131,90 +132,19 @@ func UpdateProfile(w http.ResponseWriter, r *http.Request) {
 
 // UpdateUsername will update username of giver userID
 func UpdateUsername(w http.ResponseWriter, r *http.Request) {
-	// Check content type
-	if !helpers.HasJSONRequest(r) {
-		err := helpers.ErrorResponse{
-			Message:   "Invalid content type.",
-			ErrorCode: http.StatusUnsupportedMediaType,
-		}
-		helpers.NewErrorResponse(w, &err)
-		return
-	}
-
-	var user models.User
-	userID, ok := mux.Vars(r)["id"]
-	if !ok {
-		err := helpers.ErrorResponse{
-			Message:   "Invalid User ID.",
-			ErrorCode: http.StatusUnprocessableEntity,
-		}
-		helpers.NewErrorResponse(w, &err)
-		return
-	}
-	var e error
-	user.ID, e = strconv.ParseUint(userID, 10, 64)
-	if e != nil {
-		err := helpers.ErrorResponse{
-			Message:   "Invalid User ID. Just integer values accepted",
-			ErrorCode: http.StatusUnprocessableEntity,
-		}
-		helpers.NewErrorResponse(w, &err)
-		return
-	}
-
-	//Load current user data from DB
 	db := database.NewGORMInstance()
 	defer db.Close()
-	e = db.Where("id=?", user.ID).First(&user).Error
-	if e != nil {
-		err := helpers.ErrorResponse{
-			ErrorCode: http.StatusInternalServerError,
-			Message:   "Error on loading user data.",
-		}
-		log.Println("Error on loading user data,", e)
-		helpers.NewErrorResponse(w, &err)
-
-		return
-	}
-
-	authUser, _, e := user.ExtractUserFromRequestContext(r)
-	if e != nil {
-		err := helpers.ErrorResponse{
-			ErrorCode: http.StatusUnauthorized,
-			Message:   "Auhtentication failed.",
-		}
-		log.Println("Auhtentication failed,", e)
-		helpers.NewErrorResponse(w, &err)
-
-		return
-	}
-
-	if !policies.CanEditUser(authUser, user) {
-		err := helpers.ErrorResponse{
-			ErrorCode: http.StatusForbidden,
-			Message:   "This action is not allowed for you.",
-		}
-		log.Println("This action is not allowed for you.,")
-		helpers.NewErrorResponse(w, &err)
-
-		return
-	}
-
-	if r.Body == nil {
-		err := helpers.ErrorResponse{
-			ErrorCode: http.StatusInternalServerError,
-			Message:   "Request body cant be empty",
-		}
-		log.Println("Request body cant be empty,", e)
-		helpers.NewErrorResponse(w, &err)
-
+	user, err := handleProfileSharedErrors(r, db)
+	if err != nil {
+		helpers.NewErrorResponse(w, err)
 		return
 	}
 
 	var reqModel struct {
 		Username string
 	}
-	e = json.NewDecoder(r.Body).Decode(&reqModel)
+
+	e := json.NewDecoder(r.Body).Decode(&reqModel)
 	if e != nil {
 		err := helpers.ErrorResponse{
 			ErrorCode: http.StatusInternalServerError,
@@ -258,7 +188,18 @@ func UpdateUsername(w http.ResponseWriter, r *http.Request) {
 
 	//Update
 	user.Username = reqModel.Username
-	db.Model(&user).Update(&user)
+	e = db.Model(&user).Update(&user).Error
+	if e != nil {
+		err := helpers.ErrorResponse{
+			ErrorCode: http.StatusInternalServerError,
+			Message:   "Error on updating user data.",
+		}
+		log.Println("Error on updating user data,", e)
+		helpers.NewErrorResponse(w, &err)
+
+		return
+	}
+
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&user)
 

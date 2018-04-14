391ab76d98509d0e57122b4a88dd8f8b228ad60b
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-14 23:07:18 +0430

    Add Profile view policy and Organization creating policy

--- policies/user_policy.go
+++ policies/user_policy.go
@@ -11,3 +11,19 @@ func CanEditUser(authenticatedUser models.User, requestedUser models.User) bool 
 	}
 	return false
 }
+
+//CanViewProfile check permission of authenticatedUser for viewing all details of requestedUser
+func CanViewProfile(authenticatedUser models.User, requestedUser models.User) bool {
+	if authenticatedUser.ID == requestedUser.ID || authenticatedUser.IsRootUser {
+		return true
+	}
+	return false
+}
+
+// CanCreateOrganization check permission of authenticatedUser for creating new organization
+func CanCreateOrganization(authenticatedUser models.User, requestedOrganization models.User) bool {
+	if authenticatedUser.ID == *requestedOrganization.OwnerID || authenticatedUser.IsRootUser {
+		return true
+	}
+	return false
+}

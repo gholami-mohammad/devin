03ae911c0bc8368b3b544eb402b4ca782392c41b
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-20 21:20:18 +0430

    Add doc block

--- models/user_organization.go
+++ models/user_organization.go
@@ -5,18 +5,18 @@ import (
 )
 
 type UserOrganization struct {
-	tableName           struct{} `sql:"public.user_organization"`
-	ID                  uint64
-	UserID              uint64 `doc:"ID of users record with type=1"`
-	User                *User
-	OrganizationID           uint64 `doc:"ID of users record with type=2"`
+	tableName                struct{} `sql:"public.user_organization"`
+	ID                       uint64
+	UserID                   *uint64 `doc:"ID of users record with type=1"`
+	User                     *User
+	OrganizationID           *uint64 `doc:"ID of users record with type=2"`
 	Organization             *User
-	IsAdminOfOrganization    bool `doc:"If is_admin=true => user will has full access"`
-	CanCreateProject    bool
-	CanAddUserToOrganization bool
-	CreatedByID         uint64 `doc:"Who add this user to this organization? OR How create this organization for this user?"`
-	CreatedBy           *User
-	CreatedAt           time.Time
-	UpdatedAt           time.Time
-	DeletedAt           *time.Time
+	IsAdminOfOrganization    bool   `doc:"If is_admin=true => user will has full access"`
+	CanCreateProject         bool   `doc:"Permission of adding new project to assigned organization by this user"`
+	CanAddUserToOrganization bool   `doc:"Permission of adding new user to organization by this user"`
+	CreatedByID              uint64 `doc:"Who add this user to this organization? OR How create this organization for this user?"`
+	CreatedBy                *User
+	CreatedAt                time.Time
+	UpdatedAt                time.Time
+	DeletedAt                *time.Time
 }

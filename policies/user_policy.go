85ebe812765bd7413eaa8665df60f136d96f6a42
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-21 22:36:46 +0430

    Improve permision checker

--- policies/user_policy.go
+++ policies/user_policy.go
@@ -6,10 +6,24 @@ import (
 
 //CanEditUser check permission of authenticatedUser for editing of requestedUser
 func CanEditUser(authenticatedUser models.User, requestedUser models.User) bool {
-	if authenticatedUser.ID == requestedUser.ID || authenticatedUser.IsRootUser {
-		return true
+	switch requestedUser.UserType {
+	case 1:
+		if authenticatedUser.ID == requestedUser.ID || authenticatedUser.IsRootUser {
+			return true
+		}
+		return false
+	case 2:
+		if authenticatedUser.ID == *requestedUser.OwnerID || authenticatedUser.IsRootUser {
+			return true
+		}
+		return false
+	default:
+		if authenticatedUser.ID == requestedUser.ID || authenticatedUser.IsRootUser {
+			return true
+		}
+		return false
+
 	}
-	return false
 }
 
 //CanViewProfile check permission of authenticatedUser for viewing all details of requestedUser

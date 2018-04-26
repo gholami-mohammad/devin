a6ab2f674b9bc390143c480e409759f51cefb9d3
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-26 21:37:37 +0430

    Add new data to seeder

--- database/seeders/seeders.go
+++ database/seeders/seeders.go
@@ -9,9 +9,11 @@ func SeedDB() {
 	defer db.Close()
 	db.LogMode(false)
 	db.Exec(`insert into address_countries (id, name, phone_prefix, alpha2_code, alpha3_code, locale_code) values (1, 'Iran', '+98', 'IR', 'IRR', 'fa_IR');`)
+	db.Exec(`insert into address_countries (id, name, phone_prefix, alpha2_code, alpha3_code, locale_code) values (2, 'Germany', '+49', 'DE', 'DEU', 'de_DE');`)
 
 	db.Exec(`insert into address_provinces (id, name, country_id) values (1, 'Fars', 1);`)
 	db.Exec(`insert into address_provinces (id, name, country_id) values (2, 'Tehran', 1);`)
+	db.Exec(`insert into address_provinces (id, name, country_id) values (3, 'Berlin', 2);`)
 
 	db.Exec(`insert into address_cities (id, name, country_id, province_id) values (1, 'Shiraz', 1, 1);`)
 	db.Exec(`insert into address_cities (id, name, country_id, province_id) values (2, 'Marvdasht', 1, 1);`)

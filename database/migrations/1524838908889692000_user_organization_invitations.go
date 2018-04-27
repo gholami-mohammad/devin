1f54d081383b095448bc1ade1fd0ccf71117995c
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-27 19:24:46 +0430

    Create migration for invitations

---
+++ database/migrations/1524838908889692000_user_organization_invitations.go
@@ -0,0 +1,37 @@
+package migrations
+
+import "devin/database"
+
+// Migrate the database to a new version
+func (Migration) MigrateUserOrganizationInvitations() (e error) {
+	db := database.NewGORMInstance()
+	defer db.Close()
+	e = db.Exec(`CREATE TABLE IF NOT EXISTS public.user_organization_invitations (
+        id                      bigserial NOT NULL,
+        user_id                 bigint,
+        email                   varchar(255),
+        organization_id         bigint NOT NULL,
+        accepted                bool,
+        accepted_rejected_at    timestamp with time zone,
+        created_by_id           bigint NOT NULL,
+        created_at              timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+        updated_at              timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+
+        CONSTRAINT user_organization_invitations_pkey PRIMARY KEY (id),
+        CONSTRAINT user_organization_invitations_organization_id_users_id FOREIGN KEY (organization_id)
+            REFERENCES public.users (id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+    );`).Error
+
+	return
+}
+
+// Rollback the database to previous version
+func (Migration) RollbackUserOrganizationInvitations() (e error) {
+	db := database.NewGORMInstance()
+	defer db.Close()
+	e = db.Exec(`DROP TABLE IF EXISTS public.user_organization_invitations;`).Error
+
+	return
+}

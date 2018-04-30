6ef5701a5420f15139155f9ccfe597163b7316e4
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-30 07:23:44 +0430

    Vlidate for pointer

--- helpers/validator.go
+++ helpers/validator.go
@@ -2,12 +2,16 @@ package helpers
 
 import (
 	"regexp"
+	"strings"
 )
 
 type Validator struct{}
 
 // ValidateEmailFormat check email address format.
 func (Validator) IsValidEmailFormat(email string) bool {
+	if &email == nil || strings.EqualFold(email, "") {
+		return false
+	}
 	emailRegexp := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 	return emailRegexp.MatchString(email)

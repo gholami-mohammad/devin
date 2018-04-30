21f6aeba9ec7c561a303b4f282549461a563b2b9
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-04-30 07:25:24 +0430

    Add tests of invitation

---
+++ modules/organization/controllers/invite_controller_test.go
@@ -0,0 +1,241 @@
+package controllers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+
+	"devin/middlewares"
+)
+
+func TestInviteUser(t *testing.T) {
+	_, _, tokenString := getValidUser(100, true)
+	defer deleteTestUser(100)
+	getValidOrganization(101, 100)
+	defer deleteTestOrganization(101)
+
+	path := "/api/organization/{id}/invite_user"
+	route := mux.NewRouter()
+	route.Use(middlewares.Authenticate)
+	route.HandleFunc(path, InviteUser)
+
+	t.Run("Bad Content Type", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), nil)
+		req.Header.Add("Authorization", tokenString)
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusUnsupportedMediaType {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "Invalid content type") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("No Orgaanization ID", func(t *testing.T) {
+		path := "/api/organization/invite_user"
+		route := mux.NewRouter()
+		route.Use(middlewares.Authenticate)
+		route.HandleFunc(path, InviteUser)
+
+		req, _ := http.NewRequest(http.MethodPost, path, strings.NewReader("{}"))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+		if res.StatusCode != http.StatusUnprocessableEntity {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "Invalid Organization ID") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Invalid Organization ID data type", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "ID", 1), strings.NewReader(`{}`))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+		if res.StatusCode != http.StatusUnprocessableEntity {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "integer values accepted") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Organization Not Found", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "10000999", 1), strings.NewReader(`{}`))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+		if res.StatusCode != http.StatusNotFound {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "not found") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Authentication Failed", func(t *testing.T) {
+		route := mux.NewRouter()
+		route.HandleFunc(path, InviteUser)
+
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), strings.NewReader(`{}`))
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+		if res.StatusCode != http.StatusUnauthorized {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "Auhtentication failed") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Empty Request body", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), nil)
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusInternalServerError {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "Request body cant be empty") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Bad Request body", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), strings.NewReader("Bad Content"))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusInternalServerError {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "Invalid request body") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Permission Denied", func(t *testing.T) {
+		getValidUser(2, true)
+		getValidOrganization(102, 2)
+		defer deleteTestUser(2)
+		defer deleteTestOrganization(102)
+
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "102", 1), strings.NewReader("{}"))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusForbidden {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "not permitted") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("Empty Email", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), strings.NewReader(`{}`))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusUnprocessableEntity {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "empty") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("invalid Email", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), strings.NewReader(`{"Email": "mgh"}`))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusUnprocessableEntity {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		bts, _ := ioutil.ReadAll(res.Body)
+		if !strings.Contains(string(bts), "Invalid email") {
+			t.Fatal("Invalid response message")
+		}
+	})
+
+	t.Run("OK", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodPost, strings.Replace(path, "{id}", "101", 1), strings.NewReader(`{"Email": "mgh@gmail.com"}`))
+		req.Header.Add("Authorization", tokenString)
+		req.Header.Add("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		route.ServeHTTP(rr, req)
+
+		res := rr.Result()
+
+		if res.StatusCode != http.StatusOK {
+			t.Fatal("Status code not matched. Response is", res.StatusCode)
+		}
+		defer res.Body.Close()
+		// bts, _ := ioutil.ReadAll(res.Body)
+		// if !strings.Contains(string(bts), "Invalid request body") {
+		// 	t.Fatal("Invalid response message")
+		// }
+	})
+}

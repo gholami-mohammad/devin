3a0b400c40a1160e9dff5ce05e621913f90b6f20
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-05-03 08:29:41 +0430

    Improve codes of invitation

--- modules/organization/controllers/invite_controller.go
+++ modules/organization/controllers/invite_controller.go
@@ -2,12 +2,13 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
+	"errors"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 
 	"devin/database"
 	"devin/helpers"
@@ -15,21 +16,81 @@ import (
 	"devin/policies"
 )
 
+type invitationReqModel struct {
+	Identifier string `doc:"username or email of user"`
+}
+
 // InviteUser send invitaion request to given user
 // This method invite already registered users using
 // their username or email
 func InviteUser(w http.ResponseWriter, r *http.Request) {
-	// Check content type
+	if isJsonRequest(w, r) == false {
+		return
+	}
+
+	OrganizationID, e := extractOrganizationID(w, r)
+	if e != nil {
+		return
+	}
+
+	db := database.NewGORMInstance()
+	defer db.Close()
+
+	organization, e := fetchOrganizationFromDB(w, db, OrganizationID)
+	if e != nil {
+		return
+	}
+
+	authUser, e := getAuthenticatedUser(w, r)
+	if e != nil {
+		return
+	}
+
+	if isRequestBodyNil(w, r) == true {
+		return
+	}
+
+	reqModel, e := decodeInviteRequestBody(w, r)
+	if e != nil {
+		return
+	}
+
+	if isEmailAddressEmpty(w, reqModel) == true {
+		return
+	}
+
+	if canInviteUser(w, authUser, organization) == false {
+		return
+	}
+
+	targetUser, ex := isTargetUserRegistered(w, db, reqModel)
+	if ex == false {
+		return
+	}
+
+	if saveInvitation(w, db, targetUser, OrganizationID, authUser.ID) != nil {
+		return
+	}
+
+	helpers.NewSuccessResponse(w, "Invitation sent successfully")
+}
+
+//isJsonRequest check request body for 'application/json' content type
+func isJsonRequest(w http.ResponseWriter, r *http.Request) bool {
 	if !helpers.HasJSONRequest(r) {
 		err := helpers.ErrorResponse{
 			Message:   "Invalid content type.",
 			ErrorCode: http.StatusUnsupportedMediaType,
 		}
 		helpers.NewErrorResponse(w, &err)
-		return
+		return false
 	}
 
-	// Load organization ID from URL
+	return true
+}
+
+//extractOrganizationID Load organization ID from URL
+func extractOrganizationID(w http.ResponseWriter, r *http.Request) (uint64, error) {
 	orgID, ok := mux.Vars(r)["id"]
 	if !ok {
 		err := helpers.ErrorResponse{
@@ -37,7 +98,7 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 			ErrorCode: http.StatusUnprocessableEntity,
 		}
 		helpers.NewErrorResponse(w, &err)
-		return
+		return 0, errors.New(err.Message)
 	}
 
 	OrganizationID, e := strconv.ParseUint(orgID, 10, 64)
@@ -47,39 +108,45 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 			ErrorCode: http.StatusUnprocessableEntity,
 		}
 		helpers.NewErrorResponse(w, &err)
-		return
+		return 0, errors.New(err.Message)
 	}
 
-	db := database.NewGORMInstance()
-	defer db.Close()
+	return OrganizationID, nil
+}
 
-	var organization models.User
-	//Check DB for existance of organization
-	db.Model(&organization).Where("id=? AND user_type=2", OrganizationID).First(&organization)
+// fetchOrganizationFromDB Check DB for existance of organization
+func fetchOrganizationFromDB(w http.ResponseWriter, db *gorm.DB, organizationID uint64) (organization models.User, e error) {
+	db.Model(&organization).Where("id=? AND user_type=2", organizationID).First(&organization)
 	if organization.ID == 0 {
 		err := helpers.ErrorResponse{
 			Message:   "Organization not found",
 			ErrorCode: http.StatusNotFound,
 		}
 		helpers.NewErrorResponse(w, &err)
+		e = errors.New(err.Message)
 		return
 	}
+	return
+}
 
-	// Get authenticated user
-	authUser, _, e := models.User{}.ExtractUserFromRequestContext(r)
+// getAuthenticatedUser get user who is now logged into the application
+func getAuthenticatedUser(w http.ResponseWriter, r *http.Request) (authUser models.User, e error) {
+	authUser, _, e = models.User{}.ExtractUserFromRequestContext(r)
 	if e != nil {
 		err := helpers.ErrorResponse{
 			ErrorCode: http.StatusUnauthorized,
 			Message:   "Auhtentication failed.",
 		}
 		helpers.NewErrorResponse(w, &err)
+
 		return
 	}
 
-	var reqModel struct {
-		Identifier string `doc:"username or email of user"`
-	}
+	return
+}
 
+// isRequestBodyNil check request body to being not nil
+func isRequestBodyNil(w http.ResponseWriter, r *http.Request) bool {
 	// Check request boby
 	if r.Body == nil {
 		err := helpers.ErrorResponse{
@@ -87,9 +154,14 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 			Message:   "Request body cant be empty",
 		}
 		helpers.NewErrorResponse(w, &err)
-		return
+		return true
 	}
 
+	return false
+}
+
+//decodeInviteRequestBody decode request body as invitationReqModel
+func decodeInviteRequestBody(w http.ResponseWriter, r *http.Request) (reqModel invitationReqModel, e error) {
 	// Decode request json data to model
 	e = json.NewDecoder(r.Body).Decode(&reqModel)
 	if e != nil {
@@ -102,6 +174,11 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	return
+}
+
+// isEmailAddressEmpty validate that invitation email is not empty
+func isEmailAddressEmpty(w http.ResponseWriter, reqModel invitationReqModel) bool {
 	// Check email address of null data
 	if strings.EqualFold(reqModel.Identifier, "") {
 		err := helpers.ErrorResponse{
@@ -111,8 +188,13 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 		err.Errors = make(map[string][]string)
 		err.Errors["Identifier"] = []string{"Username or email is required"}
 		helpers.NewErrorResponse(w, &err)
-		return
+		return true
 	}
+	return false
+}
+
+// canInviteUser check permission of authenticated user for invitation access
+func canInviteUser(w http.ResponseWriter, authUser, organization models.User) bool {
 
 	//Check permission of user to invite others
 	if policies.CanInviteUserToOrganization(authUser, organization) == false {
@@ -122,11 +204,16 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 		}
 		helpers.NewErrorResponse(w, &err)
 
-		return
+		return false
 	}
 
+	return true
+}
+
+// isTargetUserRegistered check invited email already registered in the application
+// It returns error if the target email address not registered already.
+func isTargetUserRegistered(w http.ResponseWriter, db *gorm.DB, reqModel invitationReqModel) (targetUser models.User, registered bool) {
 	// Check for invited user registration: already registered or not
-	var targetUser models.User
 	db.Model(&targetUser).Where("email=? OR username=?", reqModel.Identifier, reqModel.Identifier).First(&targetUser)
 	if targetUser.ID == 0 {
 		err := helpers.ErrorResponse{
@@ -136,25 +223,31 @@ func InviteUser(w http.ResponseWriter, r *http.Request) {
 		err.Errors = make(map[string][]string)
 		err.Errors["Identifier"] = []string{"No user found with this username or email "}
 		helpers.NewErrorResponse(w, &err)
+		registered = false
 		return
 	}
+	registered = true
+	return
+}
 
+// saveInvitation save final data of invitation into the DB.
+func saveInvitation(w http.ResponseWriter, db *gorm.DB, targetUser models.User, OrganizationID, CreatedByID uint64) error {
 	var invitation models.UserOrganizationInvitation
 	invitation.Email = &targetUser.Email
 	invitation.UserID = &targetUser.ID
 	invitation.OrganizationID = OrganizationID
-	invitation.CreatedByID = authUser.ID
+	invitation.CreatedByID = CreatedByID
 
-	e = db.Save(&invitation).Error
+	e := db.Save(&invitation).Error
 	if e != nil {
-		log.Fatalln(e)
 		err := helpers.ErrorResponse{
 			ErrorCode: http.StatusInternalServerError,
 			Message:   "Fail to save data",
 		}
 		helpers.NewErrorResponse(w, &err)
-		return
+		return errors.New(err.Message)
 	}
 
-	helpers.NewSuccessResponse(w, "Invitation sent successfully")
+	return nil
+
 }

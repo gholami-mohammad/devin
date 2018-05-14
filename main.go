37e0aa628d72b8a007cc36b886856cade04b5616
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-05-14 07:10:04 +0430

    Create file server on the public dicrectory

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func main() {
 	u := r.PathPrefix("/api").Subrouter().StrictSlash(true)
 	routes.LoadRoutes(u)
 
+	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         ":13000",

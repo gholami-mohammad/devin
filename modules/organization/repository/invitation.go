ca41d0c95f2582532c2354bbbb2691103c9bce85
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-05-18 10:30:49 +0430

    Add function to invitaion repo: Get pending invitaion by id Set acceptance status

--- modules/organization/repository/invitation.go
+++ modules/organization/repository/invitation.go
@@ -6,10 +6,28 @@ import (
 	"devin/models"
 )
 
+// GetPendingInvitaionsByUserID load pending invitations of user
 func GetPendingInvitaionsByUserID(db *gorm.DB, userID uint64) (pendingInvitations []models.UserOrganizationInvitation, e error) {
 	e = db.Preload("Organization").
 		Where("user_id=? AND accepted IS NULL", userID).
 		Find(&pendingInvitations).
 		Error
 	return
+}
+
+//GetPendingInvitaionsByID load single invitaion by ID
+func GetPendingInvitaionsByID(db *gorm.DB, ID uint64) (pendingInvitation models.UserOrganizationInvitation, e error) {
+	e = db.Preload("Organization").
+		Where("id=? AND accepted IS NULL", ID).
+		Find(&pendingInvitation).
+		Error
+	return
+}
+
+// SetAcceptanceStatusOfInvitaion set true for accepted or false for rejected to `accepted` column
+func SetAcceptanceStatusOfInvitaion(db *gorm.DB, ID uint64, status bool) (e error) {
+	return db.Model(&models.UserOrganizationInvitation{}).
+		Where("id=? AND accepted IS NULL").
+		UpdateColumn("accepted", status).
+		Error
 }

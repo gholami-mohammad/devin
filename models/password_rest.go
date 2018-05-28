783d141ac8a5ce857082eca57a170123087b056f
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-05-28 07:21:43 +0430

    Typo error bug fix

--- models/password_rest.go
+++ models/password_rest.go
@@ -3,16 +3,16 @@ package models
 import "time"
 
 type PasswordReset struct {
-	ID          uint64
-	UserID      uint64
-	User        *User
-	Token       string
-	UsedForRest bool
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	ExpiresAt   time.Time
+	ID           uint64
+	UserID       uint64
+	User         *User
+	Token        string
+	UsedForReset bool
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	ExpiresAt    time.Time
 }
 
 func (PasswordReset) TableName() string {
-	return "public.password_rests"
+	return "public.password_resets"
 }

1bcd23db0399ebdf313bb572b0a2ff0641cbe5e0
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-06-14 22:45:58 +0430

    Change task pointers

--- models/task.go
+++ models/task.go
@@ -9,28 +9,28 @@ type Task struct {
 	ID                      uint64
 	Title                   string
 	OrderID                 uint `doc:"شماره ترتیب قرارگیری در لیست"`
-	Description             string
-	ScheduledStartDate      time.Time
-	ScheduledCompletionDate time.Time
-	StartDate               time.Time
-	CompletionDate          time.Time `doc:"تاریخ تکمیل شدن تسک"`
-	CompletedSuccessfully   bool      `doc:"true for 'success' on completion, false for 'fail' on completion"`
-	Attachments             []*TaskAttachment
-	PriorityID              uint
+	Description             *string
+	ScheduledStartDate      *time.Time
+	ScheduledCompletionDate *time.Time
+	StartDate               *time.Time
+	CompletionDate          *time.Time `doc:"تاریخ تکمیل شدن تسک"`
+	CompletedSuccessfully   bool       `doc:"true for 'success' on completion, false for 'fail' on completion"`
+	Attachments             []TaskAttachment
+	PriorityID              *uint
 	Priority                *TaskPriority
-	FontColor               string
-	BackgroundColor         string
-	Progress                int
-	EstimatedTime           time.Duration
-	Followers               []*TaskFollower
-	PrerequisiteTasks       []*TaskPrerequisite
-	Reminders               []*TaskReminder
-	TaskBoardID             uint
+	FontColor               *string
+	BackgroundColor         *string
+	Progress                *int
+	EstimatedTime           *time.Duration
+	Followers               []TaskFollower
+	PrerequisiteTasks       []TaskPrerequisite
+	Reminders               []TaskReminder
+	TaskBoardID             *uint
 	TaskBoard               *TaskBoard
-	Tags                    []*TaggedObject `doc:"A HasMany relation, where ModuleID = models.MODULE_TASK"`
-	SpentTimes              []*TaskSpentTime
-	Comments                []*TaskComment
-	Assignments             []*TaskAssignment
+	Tags                    []TaggedObject `doc:"A HasMany relation, where ModuleID = models.MODULE_TASK"`
+	SpentTimes              []TaskSpentTime
+	Comments                []TaskComment
+	Assignments             []TaskAssignment
 	CreatedByID             uint64
 	CreatedBy               *User
 	CreatedAt               time.Time
